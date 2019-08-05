f281ccb38b739456473e2bab34f3a109d30886e8
xstater <a166224@163.com>
2019-08-05 17:25:50 +0800

    rename files

---
+++ STM32F40_41xxx/tools/i2c.h
@@ -0,0 +1,43 @@
+#ifndef _TOOLS_I2C_H_
+#define _TOOLS_I2C_H_
+
+#include "stm32f4xx.h"
+#include "delay.h"
+
+#define RETRY_COUNT_WAIT_FOR_ACK 250
+
+class I2C{
+public:
+    /**
+     * @brief initialize the I2C port and pin
+     * @note I2C must have a outside up-pull resistance which is about 4.7KOhm
+     * @param port_scl the I2C's SCL port
+     * @param pin_scl the I2C's SCL pin
+     * @param port_sda the I2C's SDA port
+     * @param pin_sda the I2C's SDA pin
+     */
+    I2C(GPIO_TypeDef *port_scl,uint16_t pin_scl,GPIO_TypeDef *port_sda,uint16_t pin_sda);
+    ~I2C();
+
+    void send_start();
+    void send_stop();
+    void send_ack();
+    void send_nack();
+    /**
+     * @brief wait for an ACK
+     * @return return true if receive a ACK,return false if receive a NACK
+     */
+    bool recv_ack();
+    void send_byte(uint8_t data);
+    uint8_t recv_byte();
+protected:
+private:
+
+
+
+    Delay &m_delay;
+    GPIO_TypeDef *m_port_scl,*m_port_sda;
+    uint16_t m_pin_scl,m_pin_sda;
+};
+
+#endif //_TOOLS_I2C_H_
